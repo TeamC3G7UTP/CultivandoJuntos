82c073b7df21a8f2193f3b8ef95fc4ad0061373e
andresfeelip29 <andreefeelip29@gmail.com>
2022-09-21 23:02:06 -0500

    Se agregan cambios a controlador

--- src/main/java/com/co/cultivemosjuntos/app/controllers/UserController.java
+++ src/main/java/com/co/cultivemosjuntos/app/controllers/UserController.java
@@ -1,10 +1,26 @@
 package com.co.cultivemosjuntos.app.controllers;
 
+import com.co.cultivemosjuntos.app.services.Business.Models.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
 public class UserController {
 
+    @PostMapping(value = "/SaveUser")
+    public String saveUser(@Valid User user, BindingResult result) {
+        //Metodo de controlador para inciar sesion
+        return "redirect:home";
+    }
 
+    @GetMapping(value = "/GetUsers")
+    public String getUsers(Model model  ) {
 
+        return "redirect:home";
+    }
 }
