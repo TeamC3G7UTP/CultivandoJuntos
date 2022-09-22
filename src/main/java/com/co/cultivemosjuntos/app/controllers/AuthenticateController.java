389452a072a62b7234eddf6736a206f34837db4d
andresfeelip29 <andreefeelip29@gmail.com>
2022-09-21 23:02:06 -0500

    Se agregan cambios a controlador

--- src/main/java/com/co/cultivemosjuntos/app/controllers/AuthenticateController.java
+++ src/main/java/com/co/cultivemosjuntos/app/controllers/AuthenticateController.java
@@ -12,12 +12,20 @@ public class AuthenticateController {
 
     @PostMapping(value = "/login")
     public String login(@Valid UserLogin userLogin, BindingResult result){
+        //Metodo de controlador para inciar sesion
+        return  "redirect:home";
+    }
 
-        if(result.hasErrors()){
-            return "modal:error";
-        }
+    @PostMapping(value = "/RequestRecovery")
+    public String requestRecovery(@Valid UserLogin userLogin, BindingResult result){
+        //Metodo de controlador para validar si se puede restablecer la contraseña del usuario
+        //si el usuario existe en base de datos
+        return  "redirect:home";
+    }
 
-        //se realiza una accion
+    @PostMapping(value = "/RecoveryPassword")
+    public String recoveryPassword(@Valid UserLogin userLogin, BindingResult result){
+        //Metodo de controlador para restablecer contraseña
         return  "redirect:home";
     }
 
