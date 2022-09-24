6cb63d5aa66a26f84b0a4cb981813539fa39648b
andresfeelip29 <andreefeelip29@gmail.com>
2022-09-24 00:06:11 -0500

    Se agregan cambios inciales al modelo para hu 10

---
+++ src/main/java/com/co/cultivemosjuntos/app/percistence/models/Entities/TypePlantEntity.java
@@ -0,0 +1,29 @@
+package com.co.cultivemosjuntos.app.percistence.models.Entities;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.io.Serial;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "TCJ_TIPO_PLANTA")
+@ToString
+public class TypePlantEntity implements Serializable {
+    @Serial
+    private  static final long serialVersionUID = 1L;
+
+    @Id
+    @Getter
+    @Setter
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Getter
+    @Setter
+    @Column(name = "id")
+    private String typeName;
+}
